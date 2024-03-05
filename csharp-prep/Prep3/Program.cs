b86581c9aca0e234040754d07e1cf17c9e68eb39
jeffpaulsav <jeffnotjefferson@gmail.com>
2024-03-05 12:04:36 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,17 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
+        int magicNumber = randomGenerator.Next(0, 10);
+        int guessNumber = 1000000;
 
-        Console.WriteLine("What is your guess? ");
-        guessNumber = Console.ReadLine();
+        while (guessNumber != magicNumber)
+        {
+            string newGuess;
+            Console.WriteLine("What is your guess? ");
+            newGuess = Console.ReadLine();
+            guessNumber = int.Parse(newGuess);
 
-        while guessNumber != magicNumber
+            if (guessNumber > magicNumber)
             {
-                Console.WriteLine("What is your guess? ")
+                Console.WriteLine("Too High ");
             }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Too Low ");
+            }
+        }
 
-        Console.WriteLine("You've guessed the number? ")
+        Console.WriteLine("You've guessed the number! ");
         
     }
 }
