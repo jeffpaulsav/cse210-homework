d4958a3ec3be7138041848fa89d8b1740ee2d237
jeffpaulsav <jeffnotjefferson@gmail.com>
2024-03-04 21:22:38 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        float grade = Console.ReadLine();
+        string letter;
+
+        if (grade >= 90)
+            {
+                letter = "A"
+                Console.WriteLine($"You got an {letter}! ");
+            }
+        else if (grade >= 80)
+            {
+                letter = "B"
+                Console.WriteLine($"You got an {letter}! ");
+            }
+        else if (grade >= 70)
+            {
+                letter = "C"
+                Console.WriteLine($"You got an {letter}! ");
+            }
+        else if (grade >= 60)
+            {
+                letter = "D"
+                Console.WriteLine($"You got an {letter}! ");
+            }
+        else
+            {
+                letter = "F"
+                Console.WriteLine($"You did not pass. Got an {letter}. ");
+            }
+
+        if grade >= 70 
+            {
+                Console.WriteLine("You've passed the class! ")
+            }
+        else
+            {
+                Console.WriteLine("Try the class again. ")
+            }
     }
 }
