8253baf7b40f8bdc6718e75ce345e7065799a4d5
jeffpaulsav <jeffnotjefferson@gmail.com>
2024-04-01 15:28:28 -0600

    Learning 05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning05
+{
+    public class Circle : Shape 
+    {
+        private double _radius;
+
+        public Circle(string color, double radius) : base(color) 
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            double math = 3.14 * (_radius * _radius);
+            return math;
+        }
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
+using Learning05;
 using System;
+using System.Threading.Channels;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("purple", 4);
+        Rectangle rectangle = new Rectangle("green", 4, 6);
+        Circle circle = new Circle("Turquoise", 5.5);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes) 
+        { 
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
+        
     }
 }
